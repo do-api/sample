0910e28a20d58714491d983f2e4e176a156dcd74
cuixx <cuixx@zbuilder.net>
2016-12-07 14:56:26 +0800

    Merge branch 'child' (reverted from commit 883a7b5ec4b47fa56ca8aa6dfcd233bf55f97c51)

--- DO-API/source/view/UI/do_SeekBar/main.ui.js
+++ DO-API/source/view/UI/do_SeekBar/main.ui.js
@@ -4,15 +4,16 @@
  * @Author : cxx
  * @Timestamp : 2016-12-06
  */ 
- 
-var target = ui("do_SeekBar_1"); 
+var app = sm("do_App");
+var page = sm("do_Page"); 
 var nf = sm("do_Notification");
 
 var do_Button_1=ui("do_Button_1");
 
 //展示相关
-do_Button_1.on("touch", function(data, e) {    
-    nf.alert("当前组件常见场景为音量调整");
+do_Button_1.on("touch", function(data, e) { 
+	var read="常见场景:\r\n音量调整\r\n\r\n非基类属性：\r\nprogress[设置第一进度值，支持手势拖动]，\r\nsecondaryProgress[设置第二进度值]\r\n\r\n非基类事件：\r\nprogressChanged[进度变化时候触发]";
+    nf.alert(read,"关于我");
 })
 
 /**
@@ -46,8 +47,7 @@ target_1.set("secondaryProgress",2);
 //点击事件
 target_1.on("touch", function(data, e) {
 	var a=target_1.progress;
-	target_1.progress=a+10; 
-	
+	target_1.progress=a+10; 	
     
 }) 
 //更改事件：进度变化时候触发
