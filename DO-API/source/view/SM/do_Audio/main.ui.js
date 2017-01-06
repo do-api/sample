bdd0f58950aff415991598cc66cc96832d057836
butterflytc <cuixxhn@163.com>
2017-01-06 18:07:06 +0800

    本地

--- DO-API/source/view/SM/do_Audio/main.ui.js
+++ DO-API/source/view/SM/do_Audio/main.ui.js
@@ -11,13 +11,13 @@ var page = sm("do_Page");
 var nf = sm("do_Notification");
 
 /** 方法
- * stopRecordAsync	结束录音	异步方法
+ * stopRecordAsync	结束录音	异步方法  ---
  * pause	暂停播放	同步方法
  * play	开始播放	同步方法
  * resume	继续播放	同步方法
- * startRecord	开始录音	同步方法
+ * startRecord	开始录音	同步方法  ---
  * stop	停止播放	同步方法
- * stopRecord	结束录音	同步方法
+ * stopRecord	结束录音	同步方法  ---
  * 
  * */
 /** 事件
@@ -31,18 +31,66 @@ var nf = sm("do_Notification");
 //录制
 var audio=sm("do_Audio");
 
-var btn1=ui("do_Button_1"); 
-btn1.on("touch",function(data){
-	audio.startRecord(path, type, quality, limit)(function(data){
-		
-		deviceone.print("录音结束");
-	});
-	
-});
-//播放
-var  btn2=ui("do_Button_2"); 
-btn2.on("touch",function(data){
-	audio.stopRecordAsync(function(data){ 
-		deviceone.print("录音结束");
+var  _path="";
+
+//开始录音
+ui("do_Button_1").on("Touch",function(data){
+	audio.startRecord({path:"data://do_Audio",type:"mp3",quality:"normal",limit:-1})(function(data){ 
+		 
+		deviceone.print("开始录音");
 	}); 
+	resultshow("开始录音"); 
 });
+//暂停录音
+ui("do_Button_2").on("Touch",function(data){
+	audio.stop();
+	resultshow("暂停录音"); 
+});
+//继续录音
+ui("do_Button_3").on("Touch",function(data){
+	audio.stop();
+	resultshow("继续录音"); 
+}); 
+//结束录音
+ui("do_Button_4").on("Touch",function(data){
+	audio.stopRecordAsync(function(data, e) {
+		
+	});
+	resultshow("结束录音"); 
+});
+
+
+//开始播放
+ui("do_Button_5").on("Touch",function(data){
+	audio.play({
+		path:_path, 
+		point:""
+	}); 
+	resultshow("开始播放"); 
+});
+//暂停播放
+ui("do_Button_6").on("Touch",function(data){
+	audio.stop();
+	resultshow("暂停播放"); 
+});
+//继续播放
+ui("do_Button_7").on("Touch",function(data){
+	audio.resume();
+	resultshow("继续播放"); 
+});
+
+//结束播放
+ui("do_Button_8").on("Touch",function(data){
+	audio.stop();
+	resultshow("结束播放"); 
+});
+ 
+
+/********************************************/
+function resultshow(res){
+	var data="当前状态："+ res;
+	ui("do_Label_3").set("Text",data);
+	
+}
+
+ 
--- DO-API/source/view/UI/do_FrameAnimationView/main.ui.js
+++ DO-API/source/view/UI/do_FrameAnimationView/main.ui.js
@@ -23,11 +23,12 @@ var target=ui("do_FrameAnimationView_1");
 ui("do_Button_1").on("touch",function(data){
 	
 	target.startGif({
-		data:"",
+		data:"source://view/UI/do_FrameAnimationView/image/15 08.21.04.gif",
 		repeat: -1
 	});
 	//data 支持data://, source://路径
 	//repeat  -1 表示无限循环
+	//15 08.21.04.gif
 	
 });
 
