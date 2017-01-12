fa41bd66c074d1c68eaa2809bb4f11312ccaf387
zhuyu0206 <258468146@qq.com>
2017-01-12 18:48:11 +0800

    richlabel1、progressbar/1/2

---
+++ DO-API/source/view/UI/do_ProgressBar/main.ui.js
@@ -0,0 +1,19 @@
+/**
+ * related to main.ui
+ * 
+ * @Author : child
+ * @Timestamp : 2017-1-12
+ */ 
+ var global = sm("do_Global");
+var app = sm("do_App");
+var page = sm("do_Page");
+var nf = sm("do_Notification");
+
+/**************/
+
+//定义ProgressBar对象
+var target_2 = ui("do_ProgressBar_2");
+
+//修改progress属性为20，只对style为horizontal有效
+target_2.progress = 20;
+
