78bc1e04216769c4768c47fa78eadb248728d0d0
butterflytc <cuixxhn@163.com>
2017-03-02 17:24:39 +0800

    indexlistview

---
+++ DO-API/source/view/UI/do_IndexListView/cell/temp.ui.js
@@ -0,0 +1,11 @@
+/**
+ * related to temp.ui
+ * 
+ * @Author : cxxf
+ * @Timestamp : 2017-03-02
+ */
+var root=ui("$");
+root.setMapping({
+    "do_Label_1.text":"text",
+    "do_Label_2.text":"text",
+});
