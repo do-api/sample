afaff3daaf424b5d4ce626c433218ff435b743a7
butterflytc <cuixxhn@163.com>
2017-03-07 10:23:03 +0800

    listview

--- DO-API/source/view/UI/do_LinearLayout/main.ui.js
+++ DO-API/source/view/UI/do_LinearLayout/main.ui.js
@@ -11,3 +11,16 @@ var page=sm("do_Page");
 page.on("back", function(data, e) {
 	app.closePage();
 })
+
+//touch	点击触发
+var target=ui("do_LinearLayout_1");
+target.on("touch",function(data){
+	
+});
+
+/*****************************************************/
+
+function reslutshow(data){
+	ui("")
+}
+
--- DO-API/source/view/UI/do_WebView/main.ui.js
+++ DO-API/source/view/UI/do_WebView/main.ui.js
@@ -18,7 +18,7 @@ var target=ui("do_WebView_1");
 /**
  * 方法
  **/
-
+ 
 
 /**
  * hello
@@ -26,16 +26,16 @@ var target=ui("do_WebView_1");
  **/
 
 target.on("failed", function(data, e) {
-	
+	resultshow("失败事件回调函数参数:"+JSON.stringify(data)+"返回值类型："+typeof(data));
 });
 
 
 target.on("loaded", function(data, e) {
-	
+	resultshow("加载结束回调函数参数:"+JSON.stringify(data)+"返回值类型："+typeof(data));
 });
 
 target.on("pull", function(data, e) {
-	
+	resultshow("pull结束回调函数参数:"+JSON.stringify(data)+"返回值类型："+typeof(data));
 });
 
 
